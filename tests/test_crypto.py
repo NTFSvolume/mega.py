1669ff8e61d34964f4f94cfb00fbe339b91bb65d
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-11-05 05:17:07 -0500

    fix: decrypt filenames with brackets

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -1,6 +1,9 @@
+from typing import Any
+
 import pytest
 
-from mega.crypto import get_chunks
+from mega.crypto import decrypt_attr, get_chunks
+from mega.data_structures import AnyArray
 
 
 @pytest.mark.parametrize(
@@ -34,3 +37,21 @@ def test_get_chunks(file_size: int, exp_result: tuple[int, int]) -> None:
     result = tuple(get_chunks(file_size))
 
     assert result == exp_result
+
+
+@pytest.mark.parametrize(
+    "attrs, key, expected_output",
+    [
+        (
+            b'\xb6E[\xe6K\xbd\xf9\t\x95\x1d\xcb\xe9\xa3N\x13 \xbb\xf3\x15\xf9H\xb7\x11\xe8\xec\\\x92"\x1d\xb45ia\xaf\x89v\xa5\xab\xd3\xb1\xa8Y\xbb\xe0\x81g\x8e\x19\x1b\xf5b\xa8\x1f`\x9d\x05b<\x13\x7fM\x07\xe0\xea',
+            (1958576006, 489861153, 2106943810, 3660715586),
+            {
+                "c": "KGUlr2utItBMvUIKW-RuwQSUdM9j",
+                "n": "SAM4}.png",
+            },
+        ),
+    ],
+)
+def test_decrypt_attr(attrs: bytes, key: AnyArray, expected_output: dict[str, Any]) -> None:
+    output = decrypt_attr(attrs, key)
+    assert output == expected_output
