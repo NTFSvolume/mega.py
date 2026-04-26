9368c37e101ed6c13806dd5a0f99c3372e721778
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2026-04-26 12:27:41 -0500

    chore: bump version

--- src/mega/__init__.py
+++ src/mega/__init__.py
@@ -4,5 +4,5 @@ from contextvars import ContextVar
 _package_name_ = "async-mega-py"
 __version__ = importlib.metadata.version(_package_name_)
 
-LOG_FILE_PROGRESS: ContextVar[bool] = ContextVar("LOG_PROGRESS", default=True)
+LOG_FILE_PROGRESS: ContextVar[bool] = ContextVar("LOG_FILE_PROGRESS", default=True)
 LOG_HTTP_TRAFFIC: ContextVar[bool] = ContextVar("LOG_HTTP_TRAFFIC", default=False)
